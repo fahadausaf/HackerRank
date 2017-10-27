8d5b323806b91354a88da574d26d1af9e88b578d
Fahad Ausaf <fahad.ausaf@icloud.com>
2017-10-27 16:27:07 +0100

    cpp:introduction: basic_data_type

---
+++ cpp/introduction/Basic_Data_Type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i;
+  long l;
+  char c;
+  float f;
+  double d;
+
+  cin >> i >> l >> c >> f >> d;
+  //cin >> l;
+
+  cout << i << "\n" << l << "\n" << c << "\n" << f << "\n" << d << endl;
+
+  return 0;
+}
