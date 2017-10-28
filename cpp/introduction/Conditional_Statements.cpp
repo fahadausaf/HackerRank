91d1c99334b33c4c207c4c77a90fbbfb281e3c79
Fahad Ausaf <fahad.ausaf@icloud.com>
2017-10-28 15:13:23 +0100

    conditional statements

---
+++ cpp/introduction/Conditional_Statements.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Conditional Statements" << endl;
+
+  int n;
+  cin >> n;
+  switch (n) {
+    case 1: std::cout << "one" << '\n'; break;
+    case 2: std::cout << "two" << '\n'; break;
+    case 3: std::cout << "three" << '\n'; break;
+    case 4: std::cout << "four" << '\n'; break;
+    case 5: std::cout << "five" << '\n'; break;
+    case 6: std::cout << "six" << '\n'; break;
+    case 7: std::cout << "seven" << '\n'; break;
+    case 8: std::cout << "eight" << '\n'; break;
+    case 9: std::cout << "nine" << '\n'; break;
+    default: std::cout << "Greater than 9" << '\n'; break;
+  }
+
+  return 0;
+}
