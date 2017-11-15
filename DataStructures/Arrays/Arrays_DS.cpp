43de9ffc14eb9c306d7ebba5e08704a183104f44
Fahad Ausaf <fahad.ausaf@icloud.com>
2017-11-15 12:24:54 +0000

    Arrays DS

---
+++ DataStructures/Arrays/Arrays_DS.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+  cout << "Arrays" << endl;
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for(int i = 0; i < n; ++i){
+    cin >> arr[i];
+  }
+
+  for(int i = n; i > 0; --i){
+    cout << arr[i-1] << " ";
+  }
+
+  return 0;
+}
