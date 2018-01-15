6a2312bb069208121499be878fd735406e33f998
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-15 21:09:47 +0000

    variable sized arrays

---
+++ cpp/introduction/Variable_Sized_Arrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+  int n, q;
+
+  cin >> n;
+  cin >> q;
+
+  vector<vector<int>> vv;
+
+  for(int i=0; i<n; i++){
+    vector<int> v;
+    int k;
+    cin >> k;
+    for(int j=0; j<k; j++){
+      int temp;
+      cin >> temp;
+      v.push_back(temp);
+    }
+    vv.push_back(v);
+  }
+
+  for(int i=0; i<q; i++){
+    int index, k;
+    cin >> index;
+    cin >> k;
+    cout << vv[index][k] << endl;
+  }
+
+  return 0;
+}
