d763f1afae68ac893dfafe3140b775a8e501f232
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-16 13:19:10 +0000

    Lower Bound STL

--- cpp/STL/Lower_Bound_STL.cpp
+++ cpp/STL/Lower_Bound_STL.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-/*
 int main(){
   int n;
   cin >> n;
@@ -14,44 +13,18 @@ int main(){
     v.push_back(tmp);
   }
 
-  int index = lower_bound(v.begin(), v.end(), 3) - v.begin();
-  cout << index << endl;
-  index = upper_bound(v.begin(), v.end(), 3) - v.begin();
-  cout << index << endl;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    int index = lower_bound (v.begin(), v.end(), x) - v.begin();
+    if (v[index] == x)
+      cout << "Yes ";
+    else
+      cout << "No ";
 
-
-  //cin >> n;
-  //for(int i=0; i<n; i++){
-  //}
-
+    cout << index + 1 << endl;
+  }
 
   return 0;
 }
-*/
-
-/*
-int main2() {
-    int n;
-    vector<int> data;
-    cin >> n;
-    while (n--) {
-        int x;
-        cin >> x;
-        data.push_back(x);
-    }
-
-    cin >> n;
-    while (n--) {
-        int x;
-        cin >> x;
-        int index = lower_bound (data.begin(), data.end(), x) - data.begin();
-        if (data[index] == x)
-            cout << "Yes ";
-        else
-            cout << "No ";
-
-        cout << index + 1 << endl;
-    }
-    return 0;
-}
-*/
