59a77686c8d5e20c4c1cfcc79286124ca4a699e6
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-18 14:07:54 +0000

    post order traversal of BST

--- DataStructures/Trees/Trees.cpp
+++ DataStructures/Trees/Trees.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 struct node
 {
     int data;
@@ -25,26 +28,38 @@ node * insert (node *root ,int value)
 }
 
 node * dummyTree(){
-  node n1 = (NULL, 1);
-  node n2 = (n1, 2);
-  node n3 = (n2, 5);
-  node n4 = (n3, 3);
-  node n5 = (n4, 6);
-  node n6 = (n5, 4);
+  node *n1 = insert(NULL, 1);
+  node *n2 = insert(n1, 2);
+  node *n3 = insert(n2, 5);
+  node *n4 = insert(n3, 3);
+  node *n5 = insert(n4, 6);
+  node *n6 = insert(n5, 4);
 
   return n6;
 }
 
 void preOrder(node *root) {
   if(root != NULL){
-    cout << root->data;
+    cout << root->data << " ";
     preOrder(root->left);
     preOrder(root->right);
   }
 }
 
+void postOrder(node *root) {
+  if(root != NULL){
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+  }
+}
+
 int main(){
   node *root = dummyTree();
+  cout << "\nPre-Order: ";
+  preOrder(root);
+  cout << "\nPost-Order: ";
+  postOrder(root);
 
   return 0;
 }
