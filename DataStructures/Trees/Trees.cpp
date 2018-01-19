f791c8099e8135405cc65109605eb07d90a95063
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-19 10:21:32 +0000

    check height os BST

--- DataStructures/Trees/Trees.cpp
+++ DataStructures/Trees/Trees.cpp
@@ -62,6 +62,21 @@ void inOrder(node *root) {
   }
 }
 
+int height(Node* root) {
+  if(root == NULL)
+    return -1;
+
+  int left = height(root->left) + 1;
+  int right = height(root->right) + 1;
+
+  if(left > right)
+    return left;
+  else
+    return right;
+
+  return 0;
+}
+
 int main(){
   node *root = dummyTree();
   cout << "\nPre-Order: ";
